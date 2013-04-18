aa961af7121e634551a20d8a219f4d2f85b0d9c5
James Bromley <james.a.bromley@googlemail.com>
2013-04-19 00:32:49 +0100

    fixing SpinnerField tests to deal with presenter increment and decrement changes

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,15 +1,12 @@
-import kikuyu.domain.Page
-import kikuyu.domain.UrlMapping
-
 class BootStrap {
 
     def init = { servletContext ->
         environments {
             development {
-                def blank = new Page(name: "blank").save(failOnError: true, flush: true)
+                /*def blank = new Page(name: "blank").save(failOnError: true, flush: true)
                 def home = new Page(name: "home").save(failOnError: true, flush: true)
                 def userAccount = new Page(name: "userAccount").save(failOnError: true, flush: true)
-                new UrlMapping(pattern: "/*", matchOrder: 1).save(failOnError: true)
+                new UrlMapping(pattern: "*//*", matchOrder: 1).save(failOnError: true)
                 new UrlMapping(pattern: "/home", matchOrder: 0, page: home).save(failOnError: true)
                 new UrlMapping(pattern: "/spock", matchOrder: 2, page: userAccount).save(failOnError: true)
                 new UrlMapping(pattern: "/kirk", matchOrder: 3, page: userAccount).save(failOnError: true)
@@ -20,7 +17,7 @@ class BootStrap {
                 new UrlMapping(pattern: "/scotty", matchOrder: 8, page: userAccount).save(failOnError: true)
                 new UrlMapping(pattern: "/bones", matchOrder: 9, page: userAccount).save(failOnError: true)
                 new UrlMapping(pattern: "/labrador", matchOrder: 10, page: blank).save(failOnError: true)
-                new UrlMapping(pattern: "/whippet", matchOrder: 11, page: blank).save(failOnError: true)
+                new UrlMapping(pattern: "/whippet", matchOrder: 11, page: blank).save(failOnError: true)*/
             }
         }
     }
